b72ebc92900b7e141db8bb07e4e9af47e4db9ee9
Pave1iOS <PGribachev94@yandex.ru>
2024-02-13 18:23:26 +0300

    add storage manager

---
+++ TaskListApp/Storage/StorageManager.swift
@@ -0,0 +1,16 @@
+//
+//  StorageManager.swift
+//  TaskListApp
+//
+//  Created by Pavel Gribachev on 13.02.2024.
+//
+
+import Foundation
+
+class StorageManager {
+    private let shared = StorageManager()
+    
+    
+    
+    private init(){}
+}
